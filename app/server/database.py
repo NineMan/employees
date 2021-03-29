4f9afabe2ec8cd0c81e6f09aa57ee72a8c863f76
Нелюбин Михаил <m.nelyubin@neovox.ru>
2021-03-29 08:48:16 +0300

    Правки dockstring'ов

--- app/server/database.py
+++ app/server/database.py
@@ -23,16 +23,23 @@ def employee_helper(employee) -> dict:
     }
 
 
-# Получить список всех сотрудников
 async def retrieve_all_employees() -> list:
+    """Получение списка всех сотрудников в БД"""
+
     employees = []
     async for employee in employees_collection.find():
         employees.append(employee_helper(employee))
     return employees
 
 
-# Добавить нового сотрудника в базу данных
 async def add_employee(employee_data: dict) -> dict:
+    """
+    Добавление нового сотрудника в коллекцию
+
+    :param employee_data:
+    :return:
+    """
+
     employee = await employees_collection.insert_one(employee_data)
     new_employee = await employees_collection.find_one(
         {"_id": employee.inserted_id}
@@ -40,20 +47,31 @@ async def add_employee(employee_data: dict) -> dict:
     return employee_helper(new_employee)
 
 
-# Получить данные сотрудника по ID
 async def retrieve_employee(id: str) -> dict:
+    """
+    Получение полных данных сотрудника по ID
+
+    :param id:
+    :return:
+    """
+
     employee = await employees_collection.find_one({"_id": ObjectId(id)})
     if employee:
         return employee_helper(employee)
 
 
-# Обновить данные сотрудника по ID
 async def update_employee(id: str, data: dict) -> bool:
-    # Возвращаем ошибку, если был послан пустой запрос
+    """
+    Обновление данных сотрудника по ID
+
+    :param id:
+    :param data:
+    :return:
+    """
+
     if len(data) < 1:
         return False
     employee = await employees_collection.find_one({"_id": ObjectId(id)})
-    print(employee)
     if employee:
         updated_employee = await employees_collection.update_one(
             {"_id": ObjectId(id)}, {"$set": data}
@@ -63,16 +81,28 @@ async def update_employee(id: str, data: dict) -> bool:
         return False
 
 
-# Удалить сотрудника из БД
 async def delete_employee(id: str) -> bool:
+    """
+    Удаление сотрудника по ID
+
+    :param id: уникальный ID в бд
+    :return:
+    """
+
     employee = await employees_collection.find_one({"_id": ObjectId(id)})
     if employee:
         await employees_collection.delete_one({"_id": ObjectId(id)})
         return True
 
 
-# Получить список сотрудников с фильтрацией по полю "gender"
 async def retrieve_employees_by_gender(gender: str) -> list:
+    """
+    Получение списка сотрудников с фильтрацией по гендеру.
+
+    :param gender: значение гендера (male/female)
+    :return: - отфильтрованный список сотрудников
+    """
+
     employees = []
 
     async for employee in employees_collection.find({"gender": gender}):
@@ -80,8 +110,14 @@ async def retrieve_employees_by_gender(gender: str) -> list:
     return employees
 
 
-# Получить список сотрудников с фильтрацией по компании
 async def retrieve_employees_by_company(company: str) -> list:
+    """
+    Получение списка сотрудников с фильтрацией по компании
+
+    :param company: - название компании
+    :return: - отфильтрованный список сотрудников
+    """
+
     employees = []
 
     async for employee in employees_collection.find({"company": company}):
@@ -89,8 +125,14 @@ async def retrieve_employees_by_company(company: str) -> list:
     return employees
 
 
-# Получить список сотрудников с фильтрацией по возрасту
 async def retrieve_employees_by_age(age: int) -> list:
+    """
+    Получение списка сотрудников с фильтрацией по возрасту
+
+    :param age: - значение возраста для фильтрации
+    :return: - отфильтрованный список сотрудников
+    """
+
     employees = []
 
     async for employee in employees_collection.find({"age": age}):
@@ -100,9 +142,14 @@ async def retrieve_employees_by_age(age: int) -> list:
 
 async def retrieve_employees_by_salary(salary: int, direction: bool) -> list:
     """
-    Получение сотрудников с фильтрацией по зарплате
-    Для фильтрации больше чем: direction=True
-    Для фильтрации меньше чем: direction=False
+    Получение списка сотрудников с фильтрацией по зарплате
+
+    Для фильтрации "больше": direction=True
+    Для фильтрации "меньше": direction=False
+
+    :param salary: - значение зарплаты
+    :param direction: - направление фильтрации (больше/меньше)
+    :return: - отфильтрованный список сотрудников
     """
 
     employees = []
--- app/server/routes/employee.py
+++ app/server/routes/employee.py
@@ -24,19 +24,29 @@ from app.server.models.employee import (
 router = APIRouter()
 
 
-# Добавление данных одного сотрудника в бд
-@router.post(
-    "/employee", response_description="Employee data added into database"
-)
+@router.post("/employee", response_description="Employee data added")
 async def add_employee_data(employee: EmployeeSchema = Body(...)):
+    """
+    Добавление данных одного сотрудника в БД
+
+    :param employee:
+    :return:
+    """
+
     employee = jsonable_encoder(employee)
     new_employee = await add_employee(employee)
     return ResponseModel(new_employee, "Employee was added successfully")
 
 
-# Получение данных одного сотрудника по ID
 @router.get("/employee/{id}", response_description="Employee data retrieved")
 async def get_employee_data(id):
+    """
+    Получение данных одного сотрудника по ID
+
+    :param id:
+    :return:
+    """
+
     employee = await retrieve_employee(id)
     if employee:
         return ResponseModel(employee, "Employee data retrieved successfully")
@@ -45,9 +55,16 @@ async def get_employee_data(id):
     )
 
 
-# Изменение данных одного сотрудника по ID
 @router.put("/employee/{id}")
 async def update_employee_data(id: str, req: UpdateEmployeeModel = Body(...)):
+    """
+    Изменение данных одного сотрудника по ID
+
+    :param id:
+    :param req:
+    :return:
+    """
+
     req = {k: v for k, v in req.dict().items() if v is not None}
     updated_employee = await update_employee(id, req)
     if updated_employee:
@@ -62,12 +79,16 @@ async def update_employee_data(id: str, req: UpdateEmployeeModel = Body(...)):
     )
 
 
-# Удаление сотрудника по ID
-@router.delete(
-    "/employee/{id}",
-    response_description="Employee data delete from the database",
-)
+@router.delete("/employee/{id}",response_description="Employee data delete")
 async def delete_employee_data(id: str):
+    """
+    Удаление сотрудника по ID
+
+
+    :param id:
+    :return:
+    """
+
     deleted_employee = await delete_employee(id)
     if deleted_employee:
         return ResponseModel(
@@ -81,9 +102,14 @@ async def delete_employee_data(id: str):
     )
 
 
-# Получение списка всех сотрудников
 @router.get("/getAllEmployees", response_description="Employees retrieved")
 async def get_all_employees():
+    """
+    Получение списка всех сотрудников
+
+    :return:
+    """
+
     employees = await retrieve_all_employees()
     if employees:
         return ResponseModel(
@@ -92,12 +118,17 @@ async def get_all_employees():
     return ResponseModel(employees, "Empty list returned")
 
 
-# Фильтрация по гендеру (параметр в пути)
 @router.get(
     "/getEmployeesByGender/{gender}",
     response_description="Employees retrieved",
 )
 async def get_employees_by_gender(gender):
+    """
+    Фильтрация по гендеру (параметр в пути)
+
+    :param gender:
+    :return:
+    """
     employees = await retrieve_employees_by_gender(gender)
     if employees:
         return ResponseModel(
@@ -106,9 +137,15 @@ async def get_employees_by_gender(gender):
     return ResponseModel(employees, "Empty list returned")
 
 
-# Фильтрация по компании (параметр в запросе)
 @router.get("/getEmploeesByCompany")
 async def get_employees_by_company(company):
+    """
+    Фильтрация по компании (параметр в запросе)
+
+    :param company:
+    :return:
+    """
+
     employees = await retrieve_employees_by_company(company)
     if employees:
         return ResponseModel(
@@ -117,9 +154,15 @@ async def get_employees_by_company(company):
     return ResponseModel(employees, "Empty list returned")
 
 
-# Фильтрация по возрасту (параметр в запросе)
 @router.get("/getEmploeesByAge")
 async def get_employees_by_age(age: int):
+    """
+    Фильтрация списка сотрудников по возрасту (параметр в запросе)
+
+    :param age:
+    :return:
+    """
+
     employees = await retrieve_employees_by_age(age)
     if employees:
         return ResponseModel(
@@ -128,9 +171,18 @@ async def get_employees_by_age(age: int):
     return ResponseModel(employees, "Empty list returned")
 
 
-# Фильтрация по зарплате. Один параметр обязательный, второй - по умолчанию
 @router.get("/getEmploeesBySalary")
 async def get_employees_by_salary(salary: int, direction: bool = True):
+    """
+    Фильтрация списка сотрудников по зарплате.
+
+    Принимает два параметра. Значение зарплаты и направление фильтрации.
+    Направление по умолчанию = True, "больше или равно"
+
+    :param salary:
+    :param direction:
+    :return:
+    """
 
     employees = await retrieve_employees_by_salary(salary, direction)
     if employees:
